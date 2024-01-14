5925b4da8fc0ec078fdfcc84bba87f9fb87e024a
shelby jenkins <shelbyjenkins@gmail.com>
2024-01-14 18:24:50 +0000

    testing base pathing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://shelbyjenkins.github.io',
-	base: 'blog',
 	integrations: [mdx(), sitemap()],
 });
