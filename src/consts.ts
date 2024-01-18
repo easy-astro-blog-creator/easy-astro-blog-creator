a45a0c9b1513c6cdf02bb768ede2212990cb8cf4
shelby jenkins <shelbyjenkins@gmail.com>
2024-01-18 00:08:55 +0000

    adds custom light/dark mode impl

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Shelby @ Github.io';
-export const SITE_DESCRIPTION = 'The personal blog of Shelby Jenkins.';
+export const NAME = 'Shelby Jenkins';
+export const SITE_TITLE = "Shelby's Blog";
+export const SITE_DESCRIPTION = `The personal blog of ${NAME}.`;
